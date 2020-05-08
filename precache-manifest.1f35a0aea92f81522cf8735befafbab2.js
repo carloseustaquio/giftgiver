08dbf1dcc3e8900724cdcd8986e95653abcc76dc
Carlos Eustáquio <caeu227@gmail.com>
2020-05-07 21:02:11 -0300

    Updates

---
+++ precache-manifest.1f35a0aea92f81522cf8735befafbab2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651330347e174e9478d1db911d6cec2a",
+    "url": "/giftgiver/index.html"
+  },
+  {
+    "revision": "432fb46a9fb092797fcf",
+    "url": "/giftgiver/static/css/main.1342f26a.chunk.css"
+  },
+  {
+    "revision": "12f3923e4adaade39dd6",
+    "url": "/giftgiver/static/js/2.54194329.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/giftgiver/static/js/2.54194329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "432fb46a9fb092797fcf",
+    "url": "/giftgiver/static/js/main.3af981da.chunk.js"
+  },
+  {
+    "revision": "7a731db9231429f4442c",
+    "url": "/giftgiver/static/js/runtime-main.ebbfe131.js"
+  }
+]);
